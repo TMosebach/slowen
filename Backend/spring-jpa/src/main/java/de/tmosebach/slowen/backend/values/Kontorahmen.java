0c4cef9723cd9f41195d096cce5194b99f52b999
ossi <Thomas.Mosebach@tmosebach.de>
2022-03-02 14:07:53 +0100

    Vorgegebene Konten definieren.

--- Backend/spring-jpa/src/main/java/de/tmosebach/slowen/backend/values/Kontorahmen.java
+++ Backend/spring-jpa/src/main/java/de/tmosebach/slowen/backend/values/Kontorahmen.java
@@ -5,6 +5,32 @@ package de.tmosebach.slowen.backend.values;
  */
 public enum Kontorahmen {
 
-	Kursgewinn,
-	Kursverlust;
+	// GuV
+	Kursgewinn("Kursgewinn"),
+	Kursverlust("Kursverlust"),
+	
+	// Gebühren
+	Bankprovision("Bankprovision"),
+	Maklercourtage("Maklercourtage"),
+	Boesenplatzentgeld("Börsenplatzentgeld"),
+	Spesen("Spesen"),
+	SpesenAusland("Spesen Ausland"),
+	SonstigeKosten("sontige Kosten"),
+	
+	// Steuer
+	Zinsabschlagsteuer("Zinsabschlagsteuer"),
+	Kapitalertragsteuer("Kapitalertragsteuer"),
+	Solidaritaetszuschlag("Solidaritätszuschlag"),
+	auslQuellensteuer("ausl. Quellensteuer"),
+	Abgeltungssteuer("Abgeltungssteuer");
+	
+	private final String name;
+
+	private Kontorahmen(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}	
 }
