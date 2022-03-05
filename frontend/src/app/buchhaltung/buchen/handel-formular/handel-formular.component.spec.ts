5a7ca0150acc8359ae07ccedb3d70c9d079707b6
ossi <Thomas.Mosebach@tmosebach.de>
2022-03-05 16:48:07 +0100

    Verkauf und Einlieferung im Handelformular eingebaut.

--- frontend/src/app/buchhaltung/buchen/handel-formular/handel-formular.component.spec.ts
+++ frontend/src/app/buchhaltung/buchen/handel-formular/handel-formular.component.spec.ts
@@ -1,22 +1,57 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { BuchhaltungService } from '../../buchhaltung.service';
+import { Buchung } from '../../domain/buchung';
+import { BuchungArt } from '../../domain/buchung-art';
 
 import { HandelFormularComponent } from './handel-formular.component';
+
+const formVorlage = {
+  art: '',
+  datum: '2022-02-25',
+  asset: 'Talanx',
+  depot: 'Depot',
+  menge: 100,
+  einheit: 'St.',
+  preis: 24,
+  waehrung: 'EUR',
+  verrechnungskonto: 'Giro',
+  valuta: '2022-02-25',
+  umsaetze: [
+    {
+      konto: 'Provision',
+      betrag: 30,
+      waehrung: 'EUR'
+    }
+  ]
+}
 
 describe('HandelFormularComponent', () => {
   let component: HandelFormularComponent;
   let fixture: ComponentFixture<HandelFormularComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ ReactiveFormsModule, HttpClientTestingModule ],
-      declarations: [ HandelFormularComponent ]
-    })
-    .compileComponents();
-  });
+  let buchhaltungService: any;
 
   beforeEach(() => {
+
+    buchhaltungService = {
+      getKonten: () => of([]),
+      buche: (buchung: Buchung) => of(buchung)
+    };
+
+    spyOn(buchhaltungService, 'buche').and.callThrough();
+
+    TestBed.configureTestingModule({
+      imports: [ ReactiveFormsModule ],
+      declarations: [ HandelFormularComponent ],
+      providers: [
+        {
+          provide: BuchhaltungService,
+          useValue: buchhaltungService
+        }
+      ]
+    });
+
     fixture = TestBed.createComponent(HandelFormularComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -25,4 +60,139 @@ describe('HandelFormularComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should deliver Kauf-Buchung', () => {
+    component.buchenForm?.setValue({
+      ...formVorlage,
+      art: 'Kauf'
+    });
+    component.doBuche();
+    
+    expect(buchhaltungService.buche).toHaveBeenCalledWith(getExpectedKauf());
+  });
+
+  it('should deliver Verkauf-Buchung', () => {
+    component.buchenForm?.setValue({
+      ...formVorlage,
+      art: 'Verkauf',
+      menge: -100,
+      preis: 30
+    });
+    component.doBuche();
+    
+    expect(buchhaltungService.buche).toHaveBeenCalledWith(getExpectedVerkauf());
+  });
+
+  it('should deliver Einlieferung-Buchung', () => {
+    component.buchenForm?.setValue({
+      ...formVorlage,
+      art: 'Einlieferung',
+      menge: 100,
+      preis: 24
+    });
+    component.doBuche();
+    
+    expect(buchhaltungService.buche).toHaveBeenCalledWith(getExpectedEinlieferung());
+  });
 });
+
+function getExpectedEinlieferung(): Buchung {
+  return {
+    art: BuchungArt.Einlieferung,
+    datum: '2022-02-25',
+    beschreibung: 'Einlieferung Talanx',
+    umsaetze: [
+      {
+        konto: 'Depot',
+        valuta: '2022-02-25',
+        betrag: {
+          betrag: 2400, 
+          waehrung: 'EUR'
+        },
+        asset: 'Talanx',
+        menge: {
+          menge: 100, 
+          einheit: 'St.'
+        }
+      }
+    ]
+  }
+}
+
+function getExpectedVerkauf(): Buchung {
+  return {
+    art: BuchungArt.Verkauf,
+    datum: '2022-02-25',
+    beschreibung: 'Verkauf Talanx',
+    umsaetze: [
+      {
+        konto: 'Depot',
+        valuta: '2022-02-25',
+        betrag: {
+          betrag: -3000, 
+          waehrung: 'EUR'
+        },
+        asset: 'Talanx',
+        menge: {
+          menge: -100, 
+          einheit: 'St.'
+        }
+      },
+      {
+        konto: 'Provision',
+        valuta: '2022-02-25',
+        betrag: {
+          betrag: 30, 
+          waehrung: 'EUR'
+        }
+      },
+      {
+        konto: 'Giro',
+        valuta: '2022-02-25',
+        betrag: {
+          betrag: 2970, 
+          waehrung: 'EUR'
+        }
+      }
+    ]
+  };  
+}
+
+function getExpectedKauf(): Buchung {
+  return {
+    art: BuchungArt.Kauf,
+    datum: '2022-02-25',
+    beschreibung: 'Kauf Talanx',
+    umsaetze: [
+      {
+        konto: 'Depot',
+        valuta: '2022-02-25',
+        betrag: {
+          betrag: 2400, 
+          waehrung: 'EUR'
+        },
+        asset: 'Talanx',
+        menge: {
+          menge: 100, 
+          einheit: 'St.'
+        }
+      },
+      {
+        konto: 'Provision',
+        valuta: '2022-02-25',
+        betrag: {
+          betrag: 30, 
+          waehrung: 'EUR'
+        }
+      },
+      {
+        konto: 'Giro',
+        valuta: '2022-02-25',
+        betrag: {
+          betrag: -2430, 
+          waehrung: 'EUR'
+        }
+      }
+    ]
+  };
+}
--- frontend/src/app/buchhaltung/buchen/handel-formular/to-buchung-conversion.ts
+++ frontend/src/app/buchhaltung/buchen/handel-formular/to-buchung-conversion.ts
@@ -5,8 +5,12 @@ import { Umsatz } from "../../domain/umsatz";
 export function transformToBuchung(formular: any): Buchung {
     switch (formular.art) {
         case 'Kauf':
-          return createKauf(formular);
-      
+            return createAssetBuchung(BuchungArt.Kauf, formular);
+        case 'Verkauf':
+            return createAssetBuchung(BuchungArt.Verkauf, formular);
+        case 'Einlieferung':
+            return createEinlieferung(formular);
+
         default:
             const message = `Unbekannte Buchungsart: ${formular.art}`;
             console.error(message);
@@ -14,31 +18,25 @@ export function transformToBuchung(formular: any): Buchung {
       }
 }
 
-/**
- * Formulareingaben in eine Buchung transformieren.
- * 
- * @param formular 
- * @returns Kauf aus Formular
- */
-export function createKauf(formular: any): Buchung {
-    const buchung: Buchung = {
-        art: BuchungArt.Kauf,
-        datum: formular.datum,
-        beschreibung: 'Kauf '+formular.asset,
-        umsaetze: []
-    };
+function createEinlieferung(formular: any): Buchung {
+    const buchung = createBasisBuchung(BuchungArt.Einlieferung, formular.datum, formular.asset);
     const umsaetze = buchung.umsaetze;
 
     const menge = Number.parseFloat(formular.menge);
     let betrag = Number.parseFloat(formular.preis) * menge;
 
-    let depotUmsatz = createUmsatz(formular.depot, formular.valuta, betrag, formular.waehrung);
-    depotUmsatz.asset = formular.asset;
-    depotUmsatz.menge = {
-        menge: menge,
-        einheit: formular.einheit
-    }
-    umsaetze?.push(depotUmsatz);
+    umsaetze?.push(createDepotUmsatz(formular, betrag, menge));
+    return buchung;
+}
+
+function createAssetBuchung(art: BuchungArt, formular: any): Buchung {
+    const buchung = createBasisBuchung(art, formular.datum, formular.asset);
+    const umsaetze = buchung.umsaetze;
+
+    const menge = Number.parseFloat(formular.menge);
+    let betrag = Number.parseFloat(formular.preis) * menge;
+
+    umsaetze?.push(createDepotUmsatz(formular, betrag, menge));
 
     // Kosten und Steuern
     formular.umsaetze.forEach( (umsatz: any) => {
@@ -50,7 +48,26 @@ export function createKauf(formular: any): Buchung {
     umsaetze?.push( createUmsatz(formular.verrechnungskonto, formular.valuta, -betrag, formular.waehrung) );
 
     return buchung;
+}
+
+function createDepotUmsatz(formular: any, betrag:number, menge: number): Umsatz {
+    let depotUmsatz = createUmsatz(formular.depot, formular.valuta, betrag, formular.waehrung);
+    depotUmsatz.asset = formular.asset;
+    depotUmsatz.menge = {
+        menge: menge,
+        einheit: formular.einheit
     }
+    return depotUmsatz;
+}
+
+function createBasisBuchung(art: BuchungArt, datum: string, asset: string): Buchung {
+    return {
+        art: art,
+        datum: datum,
+        beschreibung: `${art} ` +asset,
+        umsaetze: []
+    };
+}
 
 function createUmsatz(konto: string, valuta: string, betrag: number, waehrung: string): Umsatz {
     return {
