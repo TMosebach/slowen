50ca3e8ea1a2424018e316ac1c33b186312ccf54
ossi <Thomas.Mosebach@tmosebach.de>
2023-03-02 17:23:27 +0100

    Varianten ermöglichen

--- domains/shared/src/main/resources/db/migration/V003__AssetDomain.sql
+++
@@ -1,7 +0,0 @@
-drop table AST_ASSET if exists;
-create table AST_ASSET (
-	id VARCHAR(36) NOT NULL,
-	name VARCHAR(50) NOT NULL,
-	isin VARCHAR(12) NOT NULL,
-	wpk VARCHAR(6) NOT NULL
-);
--- domains/shared/src/main/resources/db/migration/V004__AssetBasisKonten.sql
+++
@@ -1,2 +0,0 @@
-insert into kto_konto (id, name, konto_type, bilanz_Typ, saldo, waehrung) values ('Kursverlust', 'Kursverlust', 'Konto', 'GuV', 0.0, 'EUR');
-insert into kto_konto (id, name, konto_type, bilanz_Typ, saldo, waehrung) values ('Kursgewinn', 'Kursgewinn', 'Konto', 'GuV', 0.0, 'EUR');
