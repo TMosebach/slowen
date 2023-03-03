1ef5ca2e0f8d214674105bdce7a3c5437444e702
ossi <Thomas.Mosebach@tmosebach.de>
2023-03-03 15:56:59 +0100

    Weitere Felder für die Kontoanlage

--- nodejsAngular/app/events/handler/erstelle-konto.js
+++ nodejsAngular/app/events/handler/erstelle-konto.js
@@ -1,13 +1,25 @@
 const validatoren = require('../validatoren');
 
 function erstelleKonto(command) {
-  const { name } = command;
+  const { name, kontoType, bilanzType } = command;
 
   validatoren.checkExists(name, 'Name', command);
 
+  // Todo: Zugriff auf aktulle View erst ermöglichen
+  // validatoren.checkNotKontoExists(name);
+
+  validatoren.checkExists(kontoType, 'kontoType');
+  validatoren.checkValueIsIn(kontoType, ['Konto', 'Depot']);
+
+  validatoren.checkExists(bilanzType, 'bilanzType');
+  validatoren.checkValueIsIn(bilanzType, ['Bestand', 'GuV']);
+
   return {
     eventType: 'KontoErstellt',
+    created: new Date().toISOString(),
+    kontoType,
     name,
+    bilanzType,
   };
 }
 
