de4e38c5094128ecd051f03d13d7445e217895c1
thomasmosebach <thomasmosebach@localhost>
2025-01-18 15:24:21 +0100

    Abfrage der Assetliste uns ausgabe-Verzeichnis ignorieren.

--- src/main/java/de/tmosebach/slowen/api/QueryController.java
+++ src/main/java/de/tmosebach/slowen/api/QueryController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import de.tmosebach.slowen.api.types.Asset;
 import de.tmosebach.slowen.api.types.Bestand;
 import de.tmosebach.slowen.api.types.Konto;
+import de.tmosebach.slowen.domain.AssetService;
 import de.tmosebach.slowen.domain.DepotBestand;
 import de.tmosebach.slowen.domain.KontoBestand;
 import de.tmosebach.slowen.domain.KontoService;
@@ -16,11 +18,26 @@ import de.tmosebach.slowen.values.KontoArt;
 public class QueryController {
 	
 	private KontoService kontoService;
-
-	public QueryController(KontoService kontoService) {
+	private AssetService assetService;
+	
+	public QueryController(KontoService kontoService, AssetService assetService) {
 		this.kontoService = kontoService;
+		this.assetService = assetService;
 	}
 
+	@QueryMapping
+	public List<Asset> assets() {
+		return assetService.getAssets().stream()
+				.map( domainAsset -> {
+					Asset asset = new Asset();
+					asset.setIsin(domainAsset.getIsin());
+					asset.setTyp(domainAsset.getTyp());
+					asset.setName(domainAsset.getName());
+					asset.setWpk(domainAsset.getWpk());
+					return asset;
+				}).toList();
+	}
+	
 	@QueryMapping
 	public List<String> buchungen(String konto) {
 		return List.of();
