1530815eef77420d2be2bb34b3ef6e7fb796a504
TMosebach <Thomas_Mosebach@web.de>
2026-07-19 15:03:54 +0200

    chore: Move database to workspace for development, add to .gitignore

--- electron/database/connection.ts
+++ electron/database/connection.ts
@@ -6,7 +6,9 @@ let db: Database.Database;
 
 export function getDatabase(): Database.Database {
   if (!db) {
-    const dbPath = path.join(app.getPath('userData'), 'slowen.db');
+    const dbPath = app.isPackaged
+      ? path.join(app.getPath('userData'), 'slowen.db')
+      : path.join(process.cwd(), 'slowen.db');
     db = new Database(dbPath);
     db.pragma('journal_mode = WAL');
     initDatabase();
