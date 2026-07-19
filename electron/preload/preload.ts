d5eff31239657d84420fd30ec8b8fe7468842b0a
TMosebach <Thomas_Mosebach@web.de>
2026-07-19 16:48:54 +0200

    feat: Add bookings to preload API and type declarations

--- electron/preload/preload.ts
+++ electron/preload/preload.ts
@@ -8,5 +8,12 @@ contextBridge.exposeInMainWorld('electronAPI', {
     create: (account: any) => ipcRenderer.invoke('accounts:create', account),
     update: (id: number, account: any) => ipcRenderer.invoke('accounts:update', id, account),
     delete: (id: number) => ipcRenderer.invoke('accounts:delete', id)
+  },
+  bookings: {
+    getAll: () => ipcRenderer.invoke('bookings:getAll'),
+    getById: (id: number) => ipcRenderer.invoke('bookings:getById', id),
+    create: (booking: any) => ipcRenderer.invoke('bookings:create', booking),
+    update: (id: number, booking: any) => ipcRenderer.invoke('bookings:update', id, booking),
+    delete: (id: number) => ipcRenderer.invoke('bookings:delete', id)
   }
 });
--- src/app/electron-api.d.ts
+++ src/app/electron-api.d.ts
@@ -6,6 +6,13 @@ interface ElectronAPI {
     update: (id: number, account: any) => Promise<any>;
     delete: (id: number) => Promise<void>;
   };
+  bookings: {
+    getAll: () => Promise<any[]>;
+    getById: (id: number) => Promise<any>;
+    create: (booking: any) => Promise<any>;
+    update: (id: number, booking: any) => Promise<any>;
+    delete: (id: number) => Promise<void>;
+  };
 }
 
 declare global {
