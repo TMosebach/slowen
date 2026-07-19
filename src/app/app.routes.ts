a2bdef6dd70d5e66997ffd41b424d61fe83f908d
TMosebach <Thomas_Mosebach@web.de>
2026-07-19 14:02:48 +0200

    feat: Configure routing with sidebar layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AccountListComponent } from './components/accounts/account-list/account-list.component';
+import { AccountFormComponent } from './components/accounts/account-form/account-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/accounts', pathMatch: 'full' },
+  { path: 'accounts', component: AccountListComponent },
+  { path: 'accounts/new', component: AccountFormComponent },
+  { path: 'accounts/:id/edit', component: AccountFormComponent }
+];
